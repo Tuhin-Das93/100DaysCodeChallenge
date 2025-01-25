532514d42cca49b4977d83e81785d91a33a704a8
Tuhin Das <73745558+Tuhin-Das93@users.noreply.github.com>
2025-01-25 15:01:30 +0530

    Add files via upload
    Day 1 Challenge: Band Name Generator (Basics)

---
+++ Band Name Generator.py
@@ -0,0 +1,7 @@
+## Project 1: Band Name Generator
+
+print("Hello, Welcome to the Band Name Generator")
+city_name = input("Which city did you grow up in ?\n" )
+pet_name = input("Name of your childhood pet \n" )
+
+print("Your band name is " + city_name + " " + pet_name)
